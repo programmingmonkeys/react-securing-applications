4fd31bb0d151d3b1203cd6eda8eace58a4f5ce17
codingChewie <codingChewie@gmail.com>
2021-01-05 10:04:21 -0600

    finalize menu with login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 import { useAuth0 } from '@auth0/auth0-react'
 import history from '../history'
