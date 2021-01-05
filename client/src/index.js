b2a06e1aaf012a8ab73d3defb93036580a892a0b
codingChewie <codingChewie@gmail.com>
2021-01-05 09:08:16 -0600

    update index for Auth0 integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import reportWebVitals from './reportWebVitals'
+import { Auth0Provider } from '@auth0/auth0-react'
+import config from './auth_config.json'
+import history from './history'
+
+const onRedirectCallback = appState => {
+  history.push(appState && appState.returnTo ? appState.returnTo : window.location.pathname)
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={config.domain}
+    clientId={config.clientId}
+    audience={config.audience}
+    redirectUri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root'),
 )
 
